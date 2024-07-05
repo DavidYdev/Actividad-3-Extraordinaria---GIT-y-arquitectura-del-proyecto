5a0b9db2ed1ef0af023e1ef47a2f2546152f3328
DavidYdev <davidyuhaowengwang@gmail.com>
2024-07-05 22:12:26 +0200

    CambiosdeEscena

---
+++ Assets/Scripts/BotonFinal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonFinal : MonoBehaviour
+{
+ public void Cerrar ()
+    {
+        Debug.Log("Cerrando juego");
+        Application.Quit();
+    }
+}
