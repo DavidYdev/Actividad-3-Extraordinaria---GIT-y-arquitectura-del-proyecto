2893bdd94e0bcb2a6a4994ad30d1f9ee56e690ec
DavidYdev <davidyuhaowengwang@gmail.com>
2024-07-05 22:13:07 +0200

    MovimientoPersonaje

---
+++ Assets/Scripts/CharacerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * move * speed * Time.deltaTime);
+    }
+}
